5518be8c6678651b9cd4a1ea62ff3a8a221e40da
vinicius onofre <soprosenac@gmail.com>
2025-09-02 18:31:57 -0300

    phyton UC1 Aula 12

---
+++ aula_12 _avaliacao.py
@@ -0,0 +1,48 @@
+class Carro:
+    def __init__(self, seguro, placa, reboque, cor):
+        self.seguro = seguro
+        self.placa =  placa
+        self.reboque = reboque
+        self.cor = cor
+        self._garantia = True
+        self._ligado = False
+
+    def contratar_seguro(self):
+        if self._ligado :
+            print(f"\t\t [ok] Seguro contratado")
+        else:
+            self._ligado=True 
+            print(f"\n\t [erro] Seguro ja contratado\t")
+
+    def seguro_cancelar(self):
+        if self._ligado :
+            print(f"\t\t [ok] o seguro foi cancelado")
+        else:
+            print(f"\n\t [erro] o seguro ja foi cancelado\t")
+
+    def info_cor(self):
+        if self._ligado :
+            status =  "ligado"
+        else :
+            status = "desligado"
+        print(f"{self.seguro} - {self.reboque} esta {status}")
+
+    def emplacar(self):
+        if self.placa:
+            print(f"\t\t [ok]  carro foi emplacado com a placa SRN-2019")
+
+    def reboque_instal(self):
+        if self.reboque:
+            print(f"\t\t [ok] O reboque foi instalado")
+        else:
+            print(f"\n\t [erro] o Reboque ja foi instalado\t")
+
+    def reboque_destinatario(self):
+        if self.reboque:
+            print(f"\t\t [ok] o reboque foi enviado")
+        else:
+            print(f"\n\t [erro] o reboque ja foi enviado\t")
+    
+meu_carro = Carro ("BMW", "X1", 2020, "amarelo")
+meu_carro = Carro ("chevrolet", "camaro", 2013, "azul")
+meu_carro = Carro ("audi", "A3", 2015, "ciano")
